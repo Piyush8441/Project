9098b47ca275c5e9ca8b0e1db6ef8897fb33ff39
Piyush Kumar <139025030+Piyush8441@users.noreply.github.com>
2025-03-13 12:53:45 +0530

    Update Signup.jsx
    f

--- foodHub/Frontend/src/screens/Signup.jsx
+++ foodHub/Frontend/src/screens/Signup.jsx
@@ -10,7 +10,7 @@ export default function Signup() {
   });
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://project-foodhub-frontend1.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
