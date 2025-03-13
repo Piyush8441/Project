0f02b9d717eca7b3bbfc198a90e68fc4c6f390f2
Piyush Kumar <139025030+Piyush8441@users.noreply.github.com>
2025-03-13 12:47:17 +0530

    Update Cart.jsx
    live

--- foodHub/Frontend/src/screens/Cart.jsx
+++ foodHub/Frontend/src/screens/Cart.jsx
@@ -16,7 +16,7 @@ export default function Cart() {
 
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch("https://project-foodhub-backend1.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
